3cafb499963675d22f44007c91b906e77d45dfb5
tibbi <tibor@kaputa.sk>
2016-02-16 22:57:53 +0100

    get the image paths

--- app/src/main/java/gallery/simplemobiletools/com/MainActivity.java
+++ app/src/main/java/gallery/simplemobiletools/com/MainActivity.java
@@ -1,6 +1,10 @@
 package gallery.simplemobiletools.com;
 
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getImages();
+    }
+
+    private void getImages() {
+        final ContentResolver contentResolver = getContentResolver();
+        final Uri uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+        final Cursor cursor = contentResolver.query(uri, null, null, null, null);
+
+        if (cursor != null && cursor.moveToFirst()) {
+            final int pathIndex = cursor.getColumnIndex(MediaStore.Images.Media.DATA);
+            do {
+                final String path = cursor.getString(pathIndex);
+            } while (cursor.moveToNext());
+            cursor.close();
+        }
     }
 }
