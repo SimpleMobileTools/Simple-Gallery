e3a4055fb9d8afe217d73591bfb2724662fa86fc
tibbi <tibor@kaputa.sk>
2016-02-16 22:23:25 +0100

    creating initial project

---
+++ app/src/main/java/gallery/simplemobiletools/com/MainActivity.java
@@ -0,0 +1,13 @@
+package gallery.simplemobiletools.com;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
