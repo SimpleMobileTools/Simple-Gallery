8e66c935a6a404c39ea8a133314a2384f77f34d7
tibbi <tibor@kaputa.sk>
2016-02-20 18:59:18 +0100

    make sure the background is black even at very few directories

--- app/src/main/java/gallery/simplemobiletools/com/MainActivity.java
+++ app/src/main/java/gallery/simplemobiletools/com/MainActivity.java
@@ -14,6 +14,7 @@ import java.util.Map;
 import java.util.TreeMap;
 
 public class MainActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
