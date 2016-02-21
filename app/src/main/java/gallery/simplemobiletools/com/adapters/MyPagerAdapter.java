3983428d5fd4810e28fadd915a59ca08558ba4ae
tibbi <tibor@kaputa.sk>
2016-02-21 17:50:29 +0100

    fix up displaying fullscreen images at the viewpager

--- app/src/main/java/gallery/simplemobiletools/com/adapters/MyPagerAdapter.java
+++ app/src/main/java/gallery/simplemobiletools/com/adapters/MyPagerAdapter.java
@@ -2,27 +2,22 @@ package gallery.simplemobiletools.com.adapters;
 
 import android.content.Context;
 import android.support.v4.view.PagerAdapter;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 
 import java.util.List;
 
-import gallery.simplemobiletools.com.R;
-import uk.co.senab.photoview.PhotoViewAttacher;
+import uk.co.senab.photoview.PhotoView;
 
 public class MyPagerAdapter extends PagerAdapter {
     private final Context context;
     private final List<String> paths;
-    private final LayoutInflater inflater;
 
     public MyPagerAdapter(Context context, List<String> paths) {
         this.context = context;
         this.paths = paths;
-        this.inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     }
 
     @Override
@@ -37,12 +32,10 @@ public class MyPagerAdapter extends PagerAdapter {
 
     @Override
     public Object instantiateItem(ViewGroup container, int position) {
-        final View view = inflater.inflate(R.layout.pager_item, container, false);
-        final ImageView imageView = (ImageView) view.findViewById(R.id.photo);
-        Glide.with(context).load(paths.get(position)).fitCenter().crossFade().into(imageView);
-        container.addView(view);
-        new PhotoViewAttacher(imageView);
-        return view;
+        final PhotoView photoView = new PhotoView(context);
+        Glide.with(context).load(paths.get(position)).fitCenter().crossFade().into(photoView);
+        container.addView(photoView);
+        return photoView;
     }
 
     @Override
