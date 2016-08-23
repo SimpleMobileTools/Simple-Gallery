2ce5c902047b14e114cd2f7b2008ba70b88d1648
tibbi <tibor@kaputa.sk>
2016-08-23 21:48:23 +0200

    if Same sorting is enabled, use the directory sorting

--- app/src/main/java/com/simplemobiletools/gallery/Config.java
+++ app/src/main/java/com/simplemobiletools/gallery/Config.java
@@ -39,11 +39,17 @@ public class Config {
     }
 
     public int getSorting() {
+        if (getIsSameSorting())
+            return getDirectorySorting();
+
         return mPrefs.getInt(Constants.SORT_ORDER, Constants.SORT_BY_DATE | Constants.SORT_DESCENDING);
     }
 
     public void setSorting(int order) {
-        mPrefs.edit().putInt(Constants.SORT_ORDER, order).apply();
+        if (getIsSameSorting())
+            setDirectorySorting(order);
+        else
+            mPrefs.edit().putInt(Constants.SORT_ORDER, order).apply();
     }
 
     public int getDirectorySorting() {
