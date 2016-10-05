734557d7cd74b63633db146e2ffb5c01747fe0d1
tibbi <tibor@kaputa.sk>
2016-10-05 20:47:58 +0200

    add theartofdev.edmodo:android-image-cropper for image editing

--- app/src/main/kotlin/com/simplemobiletools/gallery/activities/EditActivity.kt
+++ app/src/main/kotlin/com/simplemobiletools/gallery/activities/EditActivity.kt
@@ -1,5 +1,17 @@
 package com.simplemobiletools.gallery.activities
 
-class EditActivity : SimpleActivity() {
+import android.os.Bundle
+import com.simplemobiletools.gallery.R
+import com.theartofdev.edmodo.cropper.CropImageView
+import kotlinx.android.synthetic.main.activity_edit.*
 
+class EditActivity : SimpleActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit)
+
+        crop_image_view.apply {
+            guidelines = CropImageView.Guidelines.OFF
+        }
+    }
 }
