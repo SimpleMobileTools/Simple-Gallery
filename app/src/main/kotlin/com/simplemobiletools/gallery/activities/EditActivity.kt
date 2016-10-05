ed1a971564f93f3a94828e0da6736815d0bb57fe
tibbi <tibor@kaputa.sk>
2016-10-05 20:55:01 +0200

    add a Save menu item

--- app/src/main/kotlin/com/simplemobiletools/gallery/activities/EditActivity.kt
+++ app/src/main/kotlin/com/simplemobiletools/gallery/activities/EditActivity.kt
@@ -1,6 +1,8 @@
 package com.simplemobiletools.gallery.activities
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.simplemobiletools.gallery.R
 import com.theartofdev.edmodo.cropper.CropImageView
 import kotlinx.android.synthetic.main.activity_edit.*
@@ -13,5 +15,19 @@ class EditActivity : SimpleActivity() {
         crop_image_view.apply {
             guidelines = CropImageView.Guidelines.OFF
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_editor, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.save -> {
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
