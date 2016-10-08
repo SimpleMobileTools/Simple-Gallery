1a7c0861c3ee2533921b18ca74ec3d61a4d60c39
tibbi <tibor@kaputa.sk>
2016-10-08 17:03:52 +0200

    create an activity for setting a wallpaper

---
+++ app/src/main/kotlin/com/simplemobiletools/gallery/activities/SetWallpaperActivity.kt
@@ -0,0 +1,12 @@
+package com.simplemobiletools.gallery.activities
+
+import android.os.Bundle
+import com.simplemobiletools.gallery.R
+
+class SetWallpaperActivity : SimpleActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_set_wallpaper)
+    }
+}
