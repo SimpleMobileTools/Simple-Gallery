c524044d11398d770c85a194682f5f32e40893a9
tibbi <tibor@kaputa.sk>
2016-11-14 18:22:42 +0100

    crashfix at getRealPathFromURI

--- app/src/main/kotlin/com/simplemobiletools/gallery/extensions/context.kt
+++ app/src/main/kotlin/com/simplemobiletools/gallery/extensions/context.kt
@@ -10,9 +10,11 @@ fun Context.getRealPathFromURI(uri: Uri): String? {
     try {
         val projection = arrayOf(MediaStore.Images.Media.DATA)
         cursor = contentResolver.query(uri, projection, null, null, null)
-        val index = cursor!!.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
-        cursor.moveToFirst()
-        return cursor.getString(index)
+        if (cursor != null && cursor.moveToFirst()) {
+            val index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
+            return cursor.getString(index)
+        }
+        return null
     } finally {
         cursor?.close()
     }
