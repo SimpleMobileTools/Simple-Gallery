adadf2efec39f4bb9bf10eae811b78ded81854c2
tibbi <tibor@kaputa.sk>
2016-11-20 17:56:31 +0100

    adding "Right to Left Viewpager" License

--- app/src/main/kotlin/com/simplemobiletools/gallery/activities/LicenseActivity.kt
+++ app/src/main/kotlin/com/simplemobiletools/gallery/activities/LicenseActivity.kt
@@ -15,6 +15,7 @@ class LicenseActivity : SimpleActivity() {
         license_glide_title.setOnClickListener { openUrl(R.string.glide_url) }
         license_cropper_title.setOnClickListener { openUrl(R.string.cropper_url) }
         license_multiselect_title.setOnClickListener { openUrl(R.string.multiselect_url) }
+        license_rtl_viewpager_title.setOnClickListener { openUrl(R.string.rtl_viewpager_url) }
         license_filepicker_title.setOnClickListener { openUrl(R.string.filepicker_url) }
         license_fileproperties_title.setOnClickListener { openUrl(R.string.fileproperties_url) }
     }
