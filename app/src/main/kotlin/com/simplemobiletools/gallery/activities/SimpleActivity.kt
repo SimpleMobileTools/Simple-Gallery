7c6ba51066d27f7626b0a4c791ec8f41436bd7a4
tibbi <tibor@kaputa.sk>
2016-11-22 23:18:45 +0100

    fix #66, totally hide the navigation bar at fullscreen mode

--- app/src/main/kotlin/com/simplemobiletools/gallery/activities/SimpleActivity.kt
+++ app/src/main/kotlin/com/simplemobiletools/gallery/activities/SimpleActivity.kt
@@ -66,6 +66,7 @@ open class SimpleActivity : AppCompatActivity() {
         supportActionBar?.hide()
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
                 View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
                 View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
                 View.SYSTEM_UI_FLAG_LOW_PROFILE or
                 View.SYSTEM_UI_FLAG_FULLSCREEN or
