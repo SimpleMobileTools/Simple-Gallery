49247ee9f1e0b94b156ef39b654ba5f299add480
tibbi <tibor@kaputa.sk>
2016-12-03 21:00:55 +0100

    wrap the sorting dialog in a scrollview

--- app/src/main/kotlin/com/simplemobiletools/gallery/dialogs/ChangeSortingDialog.kt
+++ app/src/main/kotlin/com/simplemobiletools/gallery/dialogs/ChangeSortingDialog.kt
@@ -34,36 +34,36 @@ class ChangeSortingDialog(val activity: Activity, val isDirectorySorting: Boolea
     }
 
     private fun setupSortRadio() {
-        val sortingRadio = view.dialog_radio_sorting
-        var sortBtn = sortingRadio.dialog_radio_name
+        val sortingRadio = view.sorting_dialog_radio_sorting
+        var sortBtn = sortingRadio.sorting_dialog_radio_name
 
         if (currSorting and SORT_BY_DATE != 0) {
-            sortBtn = sortingRadio.dialog_radio_date
+            sortBtn = sortingRadio.sorting_dialog_radio_date
         } else if (currSorting and SORT_BY_SIZE != 0) {
-            sortBtn = sortingRadio.dialog_radio_size
+            sortBtn = sortingRadio.sorting_dialog_radio_size
         }
         sortBtn.isChecked = true
     }
 
     private fun setupOrderRadio() {
-        val orderRadio = view.dialog_radio_order
-        var orderBtn = orderRadio.dialog_radio_ascending
+        val orderRadio = view.sorting_dialog_radio_order
+        var orderBtn = orderRadio.sorting_dialog_radio_ascending
 
         if (currSorting and SORT_DESCENDING != 0) {
-            orderBtn = orderRadio.dialog_radio_descending
+            orderBtn = orderRadio.sorting_dialog_radio_descending
         }
         orderBtn.isChecked = true
     }
 
     override fun onClick(dialog: DialogInterface, which: Int) {
-        val sortingRadio = view.dialog_radio_sorting
+        val sortingRadio = view.sorting_dialog_radio_sorting
         var sorting = when (sortingRadio.checkedRadioButtonId) {
-            R.id.dialog_radio_name -> SORT_BY_NAME
-            R.id.dialog_radio_date -> SORT_BY_DATE
+            R.id.sorting_dialog_radio_name -> SORT_BY_NAME
+            R.id.sorting_dialog_radio_date -> SORT_BY_DATE
             else -> SORT_BY_SIZE
         }
 
-        if (view.dialog_radio_order.checkedRadioButtonId == R.id.dialog_radio_descending) {
+        if (view.sorting_dialog_radio_order.checkedRadioButtonId == R.id.sorting_dialog_radio_descending) {
             sorting = sorting or SORT_DESCENDING
         }
 
