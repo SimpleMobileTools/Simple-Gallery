6ba436db805454b42fc74abf18e0e880aee16c90
tibbi <tibor@kaputa.sk>
2016-12-27 20:06:38 +0100

    Improve the performance by removing file validity check

--- app/src/main/kotlin/com/simplemobiletools/gallery/asynctasks/GetDirectoriesAsynctask.kt
+++ app/src/main/kotlin/com/simplemobiletools/gallery/asynctasks/GetDirectoriesAsynctask.kt
@@ -54,7 +54,7 @@ class GetDirectoriesAsynctask(val context: Context, val isPickVideo: Boolean, va
                         val file = File(fullPath)
                         val size = cursor.getLongValue(MediaStore.Images.Media.SIZE)
 
-                        if (size == 0L || !file.exists()) {
+                        if (size == 0L) {
                             invalidFiles.add(file)
                             continue
                         }
