e9b40a6c1307a8f101bf5f6bd16c8e1294a999ae
tibbi <tibor@kaputa.sk>
2017-01-07 12:08:10 +0100

    add zooming in whatsnew

--- app/src/main/kotlin/com/simplemobiletools/gallery/activities/MainActivity.kt
+++ app/src/main/kotlin/com/simplemobiletools/gallery/activities/MainActivity.kt
@@ -372,6 +372,7 @@ class MainActivity : SimpleActivity(), DirectoryAdapter.DirOperationsListener {
             add(Release(51, R.string.release_51))
             add(Release(52, R.string.release_52))
             add(Release(54, R.string.release_54))
+            add(Release(58, R.string.release_58))
             checkWhatsNew(this, BuildConfig.VERSION_CODE)
         }
     }
