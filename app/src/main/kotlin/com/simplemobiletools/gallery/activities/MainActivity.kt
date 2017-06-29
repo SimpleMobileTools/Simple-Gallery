e7dd7185e2d4b99e2766d8be0d989a9f3f534d95
tibbi <tibor@kaputa.sk>
2017-06-29 19:05:49 +0200

    updating release notes

--- app/src/main/kotlin/com/simplemobiletools/gallery/activities/MainActivity.kt
+++ app/src/main/kotlin/com/simplemobiletools/gallery/activities/MainActivity.kt
@@ -474,6 +474,7 @@ class MainActivity : SimpleActivity(), DirectoryAdapter.DirOperationsListener {
             add(Release(108, R.string.release_108))
             add(Release(112, R.string.release_112))
             add(Release(114, R.string.release_114))
+            add(Release(115, R.string.release_115))
             checkWhatsNew(this, BuildConfig.VERSION_CODE)
         }
     }
