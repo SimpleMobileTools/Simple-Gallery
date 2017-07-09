3df76261e0edd74ba6c9dd79c86ac967265ad59e
tibbi <tibor@kaputa.sk>
2017-07-09 09:28:26 +0200

    fix #302, add a toggle for disabling leak canary

--- app/src/main/kotlin/com/simplemobiletools/gallery/App.kt
+++ app/src/main/kotlin/com/simplemobiletools/gallery/App.kt
@@ -4,11 +4,14 @@ import android.app.Application
 import com.squareup.leakcanary.LeakCanary
 
 class App : Application() {
+    val USE_LEAK_CANARY = false
     override fun onCreate() {
         super.onCreate()
-        if (LeakCanary.isInAnalyzerProcess(this)) {
-            return
+        if (USE_LEAK_CANARY) {
+            if (LeakCanary.isInAnalyzerProcess(this)) {
+                return
+            }
+            LeakCanary.install(this)
         }
-        LeakCanary.install(this)
     }
 }
