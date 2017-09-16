f7e7d83382b92888d21fabbebd3ce3962f2ee67e
tibbi <tibor@kaputa.sk>
2017-09-16 20:20:43 +0200

    do not force ARGB_8888 format at fullscreen media

--- app/src/main/kotlin/com/simplemobiletools/gallery/helpers/GlideDecoder.kt
+++ app/src/main/kotlin/com/simplemobiletools/gallery/helpers/GlideDecoder.kt
@@ -8,7 +8,6 @@ import android.graphics.drawable.Drawable
 import android.media.ExifInterface
 import android.net.Uri
 import com.bumptech.glide.Glide
-import com.bumptech.glide.load.DecodeFormat
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.request.RequestOptions
 import com.bumptech.glide.request.target.Target
@@ -26,7 +25,6 @@ class GlideDecoder : ImageDecoder {
 
         val options = RequestOptions()
                 .signature(uri.path.getFileSignature())
-                .format(DecodeFormat.PREFER_ARGB_8888)
                 .diskCacheStrategy(DiskCacheStrategy.RESOURCE)
                 .override(targetWidth, targetHeight)
 
