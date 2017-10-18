f0159843851dc965e4769ccff4034510f09521ed
tibbi <tibor@kaputa.sk>
2017-10-18 17:41:20 +0200

    remove a Directory helper function addSize

--- app/src/main/kotlin/com/simplemobiletools/gallery/models/Directory.kt
+++ app/src/main/kotlin/com/simplemobiletools/gallery/models/Directory.kt
@@ -4,14 +4,10 @@ import com.simplemobiletools.commons.helpers.*
 import java.io.Serializable
 
 data class Directory(val path: String, val tmb: String, val name: String, var mediaCnt: Int, val modified: Long, val taken: Long,
-                     var size: Long) : Serializable, Comparable<Directory> {
+                     val size: Long) : Serializable, Comparable<Directory> {
     companion object {
         private val serialVersionUID = -6553345863555455L
         var sorting: Int = 0
-    }
-
-    fun addSize(bytes: Long) {
-        size += bytes
     }
 
     override fun compareTo(other: Directory): Int {
