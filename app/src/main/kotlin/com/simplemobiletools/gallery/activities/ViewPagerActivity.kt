b1019af51b34c8cae3eb50f58acc733d18798d61
tibbi <tibor@kaputa.sk>
2017-10-28 18:39:26 +0200

    fix #320, make orientation by "Device rotation" less sensitive

--- app/src/main/kotlin/com/simplemobiletools/gallery/activities/ViewPagerActivity.kt
+++ app/src/main/kotlin/com/simplemobiletools/gallery/activities/ViewPagerActivity.kt
@@ -176,8 +176,8 @@ class ViewPagerActivity : SimpleActivity(), ViewPager.OnPageChangeListener, View
         mOrientationEventListener = object : OrientationEventListener(this, SensorManager.SENSOR_DELAY_NORMAL) {
             override fun onOrientationChanged(orientation: Int) {
                 val currOrient = when (orientation) {
-                    in 45..134 -> ORIENT_LANDSCAPE_RIGHT
-                    in 225..314 -> ORIENT_LANDSCAPE_LEFT
+                    in 75..134 -> ORIENT_LANDSCAPE_RIGHT
+                    in 225..289 -> ORIENT_LANDSCAPE_LEFT
                     else -> ORIENT_PORTRAIT
                 }
 
