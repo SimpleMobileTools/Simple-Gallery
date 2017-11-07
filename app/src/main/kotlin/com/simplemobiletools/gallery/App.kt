54dca20bdbe906837dbe703287d3ff17afe814f4
tibbi <tibor@kaputa.sk>
2017-11-07 17:31:40 +0100

    initialize leak canary in debug build

--- app/src/main/kotlin/com/simplemobiletools/gallery/App.kt
+++ app/src/main/kotlin/com/simplemobiletools/gallery/App.kt
@@ -2,18 +2,19 @@ package com.simplemobiletools.gallery
 
 import android.app.Application
 import com.github.ajalt.reprint.core.Reprint
+import com.simplemobiletools.gallery.BuildConfig.USE_LEAK_CANARY
 import com.squareup.leakcanary.LeakCanary
 
 class App : Application() {
-    val USE_LEAK_CANARY = false
     override fun onCreate() {
         super.onCreate()
-        Reprint.initialize(this)
         if (USE_LEAK_CANARY) {
             if (LeakCanary.isInAnalyzerProcess(this)) {
                 return
             }
             LeakCanary.install(this)
         }
+
+        Reprint.initialize(this)
     }
 }
