15daa64cbd6ebba6d4f6868f7009b2c92e4d72e7
tibbi <tibor@kaputa.sk>
2017-11-27 20:32:57 +0100

    reenable leak canary

--- app/src/main/kotlin/com/simplemobiletools/gallery/App.kt
+++ app/src/main/kotlin/com/simplemobiletools/gallery/App.kt
@@ -2,18 +2,20 @@ package com.simplemobiletools.gallery
 
 import android.support.multidex.MultiDexApplication
 import com.github.ajalt.reprint.core.Reprint
+import com.simplemobiletools.gallery.BuildConfig.USE_LEAK_CANARY
 import com.simplemobiletools.gallery.extensions.config
+import com.squareup.leakcanary.LeakCanary
 import java.util.*
 
 class App : MultiDexApplication() {
     override fun onCreate() {
         super.onCreate()
-        /*if (USE_LEAK_CANARY) {
+        if (USE_LEAK_CANARY) {
             if (LeakCanary.isInAnalyzerProcess(this)) {
                 return
             }
             LeakCanary.install(this)
-        }*/
+        }
 
         if (config.useEnglish) {
             val conf = resources.configuration
