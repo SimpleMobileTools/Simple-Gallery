a56bbbd54a95e2da9aad3c8aa16f07fd19d7b5c1
tibbi <tibor@kaputa.sk>
2017-12-01 23:35:36 +0100

    simplify Leak Canary initialization

--- app/src/main/kotlin/com/simplemobiletools/gallery/App.kt
+++ app/src/main/kotlin/com/simplemobiletools/gallery/App.kt
@@ -3,13 +3,12 @@ package com.simplemobiletools.gallery
 import android.support.multidex.MultiDexApplication
 import com.github.ajalt.reprint.core.Reprint
 import com.simplemobiletools.commons.extensions.checkUseEnglish
-import com.simplemobiletools.gallery.BuildConfig.USE_LEAK_CANARY
 import com.squareup.leakcanary.LeakCanary
 
 class App : MultiDexApplication() {
     override fun onCreate() {
         super.onCreate()
-        if (USE_LEAK_CANARY) {
+        if (BuildConfig.DEBUG) {
             if (LeakCanary.isInAnalyzerProcess(this)) {
                 return
             }
