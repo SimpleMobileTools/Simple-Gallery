42408491516ec1192013bcdccece473bff32cc9e
tibbi <tibor@kaputa.sk>
2017-12-29 16:46:13 +0100

    add an activity for displaying hidden folders

--- app/src/main/kotlin/com/simplemobiletools/gallery/activities/HiddenFoldersActivity.kt
+++ app/src/main/kotlin/com/simplemobiletools/gallery/activities/HiddenFoldersActivity.kt
@@ -5,14 +5,13 @@ import android.view.Menu
 import android.view.MenuItem
 import com.simplemobiletools.commons.dialogs.FilePickerDialog
 import com.simplemobiletools.commons.extensions.beVisibleIf
-import com.simplemobiletools.commons.extensions.scanPath
 import com.simplemobiletools.commons.interfaces.RefreshRecyclerViewListener
 import com.simplemobiletools.gallery.R
-import com.simplemobiletools.gallery.adapters.ManageFoldersAdapter
 import com.simplemobiletools.gallery.extensions.config
+import com.simplemobiletools.gallery.extensions.getNoMediaFolders
 import kotlinx.android.synthetic.main.activity_manage_folders.*
 
-class IncludedFoldersActivity : SimpleActivity(), RefreshRecyclerViewListener {
+class HiddenFoldersActivity : SimpleActivity(), RefreshRecyclerViewListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_manage_folders)
@@ -20,17 +19,12 @@ class IncludedFoldersActivity : SimpleActivity(), RefreshRecyclerViewListener {
     }
 
     private fun updateFolders() {
-        val folders = ArrayList<String>()
-        config.includedFolders.mapTo(folders, { it })
+        val folders = getNoMediaFolders()
         manage_folders_placeholder.apply {
-            text = getString(R.string.included_activity_placeholder)
+            text = getString(R.string.hidden_folders_placeholder)
             beVisibleIf(folders.isEmpty())
             setTextColor(config.textColor)
         }
-
-        val adapter = ManageFoldersAdapter(this, folders, false, this, manage_folders_list) {}
-        adapter.setupDragListener(true)
-        manage_folders_list.adapter = adapter
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -52,11 +46,7 @@ class IncludedFoldersActivity : SimpleActivity(), RefreshRecyclerViewListener {
 
     private fun addFolder() {
         FilePickerDialog(this, pickFile = false, showHidden = config.shouldShowHidden) {
-            config.addIncludedFolder(it)
-            updateFolders()
-            Thread {
-                scanPath(it)
-            }.start()
+
         }
     }
 }
