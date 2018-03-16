199a0a2407e69272ff7d0a1c1350a76ac329d171
tibbi <tibor@kaputa.sk>
2018-03-16 11:40:13 +0100

    add image cropping in FAQ

--- app/src/main/kotlin/com/simplemobiletools/gallery/extensions/Activity.kt
+++ app/src/main/kotlin/com/simplemobiletools/gallery/extensions/Activity.kt
@@ -83,6 +83,7 @@ fun SimpleActivity.launchAbout() {
             FAQItem(R.string.faq_7_title, R.string.faq_7_text),
             FAQItem(R.string.faq_8_title, R.string.faq_8_text),
             FAQItem(R.string.faq_9_title, R.string.faq_9_text),
+            FAQItem(R.string.faq_10_title, R.string.faq_10_text),
             FAQItem(R.string.faq_2_title_commons, R.string.faq_2_text_commons))
 
     startAboutActivity(R.string.app_name, LICENSE_GLIDE or LICENSE_CROPPER or LICENSE_MULTISELECT or LICENSE_RTL
