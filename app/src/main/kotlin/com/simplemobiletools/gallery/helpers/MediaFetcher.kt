ca97779f5d809ddcaada9147c829c3bebbf6be9d
tibbi <tibor@kaputa.sk>
2018-03-17 18:19:33 +0100

    try not scanning files from explicitly included folders

--- app/src/main/kotlin/com/simplemobiletools/gallery/helpers/MediaFetcher.kt
+++ app/src/main/kotlin/com/simplemobiletools/gallery/helpers/MediaFetcher.kt
@@ -303,7 +303,7 @@ class MediaFetcher(val context: Context) {
             val isAlreadyAdded = curMedia.any { it.path == file.absolutePath }
             if (!isAlreadyAdded) {
                 curMedia.add(medium)
-                context.scanPath(file.absolutePath)
+                //context.scanPath(file.absolutePath)
             }
         }
     }
