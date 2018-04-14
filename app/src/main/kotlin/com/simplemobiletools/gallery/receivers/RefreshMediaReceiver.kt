870b8c6837268a18951609ec5644b60c8c5ef7ad
tibbi <tibor@kaputa.sk>
2018-04-14 23:02:01 +0200

    sublist directories at background update

--- app/src/main/kotlin/com/simplemobiletools/gallery/receivers/RefreshMediaReceiver.kt
+++ app/src/main/kotlin/com/simplemobiletools/gallery/receivers/RefreshMediaReceiver.kt
@@ -6,11 +6,13 @@ import android.content.Intent
 import com.google.gson.Gson
 import com.simplemobiletools.gallery.asynctasks.GetDirectoriesAsynctask
 import com.simplemobiletools.gallery.extensions.config
+import com.simplemobiletools.gallery.helpers.SAVE_DIRS_CNT
 
 class RefreshMediaReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
         GetDirectoriesAsynctask(context, false, false) {
-            context.config.directories = Gson().toJson(it)
+            val subList = it.subList(0, Math.min(SAVE_DIRS_CNT, it.size))
+            context.config.directories = Gson().toJson(subList)
         }.execute()
     }
 }
