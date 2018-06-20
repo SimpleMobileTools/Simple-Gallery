a76c716173a9e156405a53c43e532fb1398f11cf
tibbi <tibor@kaputa.sk>
2018-06-20 23:22:46 +0200

    add Favorites to release notes

--- app/src/main/kotlin/com/simplemobiletools/gallery/activities/MainActivity.kt
+++ app/src/main/kotlin/com/simplemobiletools/gallery/activities/MainActivity.kt
@@ -910,6 +910,7 @@ class MainActivity : SimpleActivity(), DirectoryAdapter.DirOperationsListener {
             add(Release(177, R.string.release_177))
             add(Release(178, R.string.release_178))
             add(Release(180, R.string.release_180))
+            add(Release(181, R.string.release_181))
             checkWhatsNew(this, BuildConfig.VERSION_CODE)
         }
     }
