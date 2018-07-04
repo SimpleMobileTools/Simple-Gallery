ed7e1a6d026496325bb09f7abdb02307a747c665
tibbi <tibor@kaputa.sk>
2018-07-04 11:10:12 +0200

    adding Panorama view and Sanselan licenses

--- app/src/main/kotlin/com/simplemobiletools/gallery/extensions/Activity.kt
+++ app/src/main/kotlin/com/simplemobiletools/gallery/extensions/Activity.kt
@@ -75,7 +75,8 @@ fun SimpleActivity.launchAbout() {
             FAQItem(R.string.faq_2_title_commons, R.string.faq_2_text_commons))
 
     startAboutActivity(R.string.app_name, LICENSE_GLIDE or LICENSE_CROPPER or LICENSE_MULTISELECT or LICENSE_RTL
-            or LICENSE_SUBSAMPLING or LICENSE_PATTERN or LICENSE_REPRINT or LICENSE_GIF_DRAWABLE or LICENSE_PHOTOVIEW or LICENSE_EXOPLAYER, BuildConfig.VERSION_NAME, faqItems)
+            or LICENSE_SUBSAMPLING or LICENSE_PATTERN or LICENSE_REPRINT or LICENSE_GIF_DRAWABLE or LICENSE_PHOTOVIEW or LICENSE_EXOPLAYER or
+            LICENSE_PANORAMA_VIEW or LICENSE_SANSELAN, BuildConfig.VERSION_NAME, faqItems)
 }
 
 fun AppCompatActivity.showSystemUI(toggleActionBarVisibility: Boolean) {
