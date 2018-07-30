8e23c54978a2f10417d9401963fd3be0e6f7c884
tibbi <tibor@kaputa.sk>
2018-07-30 15:38:22 +0200

    update commons to 4.5.12

--- app/src/main/kotlin/com/simplemobiletools/gallery/extensions/Activity.kt
+++ app/src/main/kotlin/com/simplemobiletools/gallery/extensions/Activity.kt
@@ -79,7 +79,7 @@ fun SimpleActivity.launchAbout() {
     val licenses = LICENSE_GLIDE or LICENSE_CROPPER or LICENSE_MULTISELECT or LICENSE_RTL or LICENSE_SUBSAMPLING or LICENSE_PATTERN or
             LICENSE_REPRINT or LICENSE_GIF_DRAWABLE or LICENSE_PHOTOVIEW or LICENSE_EXOPLAYER or LICENSE_PANORAMA_VIEW or LICENSE_SANSELAN or LICENSE_FILTERS
 
-    startAboutActivity(R.string.app_name, licenses, BuildConfig.VERSION_NAME, faqItems)
+    startAboutActivity(R.string.app_name, licenses, BuildConfig.VERSION_NAME, faqItems, true)
 }
 
 fun AppCompatActivity.showSystemUI(toggleActionBarVisibility: Boolean) {
