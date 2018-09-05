d0c612b3fedd18e8bd2d68619c6fd54112c20196
tibbi <tibor@kaputa.sk>
2018-09-05 13:45:29 +0200

    removing LeakCanary

--- app/src/main/kotlin/com/simplemobiletools/gallery/App.kt
+++ app/src/main/kotlin/com/simplemobiletools/gallery/App.kt
@@ -3,18 +3,10 @@ package com.simplemobiletools.gallery
 import android.support.multidex.MultiDexApplication
 import com.github.ajalt.reprint.core.Reprint
 import com.simplemobiletools.commons.extensions.checkUseEnglish
-import com.squareup.leakcanary.LeakCanary
 
 class App : MultiDexApplication() {
     override fun onCreate() {
         super.onCreate()
-        if (BuildConfig.DEBUG) {
-            if (LeakCanary.isInAnalyzerProcess(this)) {
-                return
-            }
-            LeakCanary.install(this)
-        }
-
         checkUseEnglish()
         Reprint.initialize(this)
     }
