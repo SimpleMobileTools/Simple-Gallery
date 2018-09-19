730ca8b5c09e4a536a7a3541e78f0ba64ea77144
tibbi <tibor@kaputa.sk>
2018-09-19 15:53:44 +0200

    add batch deleting of invalid files from room DB

--- app/src/main/kotlin/com/simplemobiletools/gallery/interfaces/MediumDao.kt
+++ app/src/main/kotlin/com/simplemobiletools/gallery/interfaces/MediumDao.kt
@@ -1,6 +1,7 @@
 package com.simplemobiletools.gallery.interfaces
 
 import android.arch.persistence.room.Dao
+import android.arch.persistence.room.Delete
 import android.arch.persistence.room.Insert
 import android.arch.persistence.room.OnConflictStrategy.REPLACE
 import android.arch.persistence.room.Query
@@ -25,6 +26,9 @@ interface MediumDao {
 
     @Insert(onConflict = REPLACE)
     fun insertAll(media: List<Medium>)
+
+    @Delete
+    fun deleteMedia(vararg medium: Medium)
 
     @Query("DELETE FROM media WHERE full_path = :path COLLATE NOCASE")
     fun deleteMediumPath(path: String)
