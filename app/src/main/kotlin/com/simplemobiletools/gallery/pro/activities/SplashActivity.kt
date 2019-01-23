e34fe1c78fe87aaafbc94eb56e5d62ece46ab62c
tibbi <tibor@kaputa.sk>
2019-01-23 15:04:19 +0100

    lets skip the sideloading check for now

--- app/src/main/kotlin/com/simplemobiletools/gallery/pro/activities/SplashActivity.kt
+++ app/src/main/kotlin/com/simplemobiletools/gallery/pro/activities/SplashActivity.kt
@@ -4,7 +4,7 @@ import android.content.Intent
 import com.simplemobiletools.commons.activities.BaseSplashActivity
 
 class SplashActivity : BaseSplashActivity() {
-    override fun getAppPackageName() = packageName
+    override fun getAppPackageName() = "-1"
 
     override fun initActivity() {
         startActivity(Intent(this, MainActivity::class.java))
