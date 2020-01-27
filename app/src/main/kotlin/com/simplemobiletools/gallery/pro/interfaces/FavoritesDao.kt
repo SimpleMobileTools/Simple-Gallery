665690c81912ec993f5512ec3f515319b3a9893f
tibbi <tibor@kaputa.sk>
2020-01-27 20:34:59 +0100

    removing an unused debug function

--- app/src/main/kotlin/com/simplemobiletools/gallery/pro/interfaces/FavoritesDao.kt
+++ app/src/main/kotlin/com/simplemobiletools/gallery/pro/interfaces/FavoritesDao.kt
@@ -1,6 +1,9 @@
 package com.simplemobiletools.gallery.pro.interfaces
 
-import androidx.room.*
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
 import com.simplemobiletools.gallery.pro.models.Favorite
 
 @Dao
@@ -10,9 +13,6 @@ interface FavoritesDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertAll(favorites: List<Favorite>)
-
-    @Query("SELECT full_path FROM favorites")
-    fun getFavoriteRawPaths(): List<String>
 
     @Query("SELECT favorites.full_path FROM favorites INNER JOIN media ON favorites.full_path = media.full_path WHERE media.deleted_ts = 0")
     fun getValidFavoritePaths(): List<String>
