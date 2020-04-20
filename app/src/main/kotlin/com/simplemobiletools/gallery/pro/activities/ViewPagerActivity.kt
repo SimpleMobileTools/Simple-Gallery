a1e0d077985a897e5a94d22ad53c95abb51e92d4
tibbi <tibor@kaputa.sk>
2020-04-20 20:11:57 +0200

    fix a glitch with fullscreen view bottom actions being at the top

--- app/src/main/kotlin/com/simplemobiletools/gallery/pro/activities/ViewPagerActivity.kt
+++ app/src/main/kotlin/com/simplemobiletools/gallery/pro/activities/ViewPagerActivity.kt
@@ -758,7 +758,8 @@ class ViewPagerActivity : SimpleActivity(), ViewPager.OnPageChangeListener, View
     }
 
     private fun initBottomActionsLayout() {
-        bottom_actions.layoutParams.height = resources.getDimension(R.dimen.bottom_actions_height).toInt() + navigationBarHeight
+        val useNavigationBarHeight = if (navigationBarBottom) navigationBarHeight else 0
+        bottom_actions.layoutParams.height = resources.getDimension(R.dimen.bottom_actions_height).toInt() + useNavigationBarHeight
         if (config.bottomActions) {
             bottom_actions.beVisible()
         } else {
