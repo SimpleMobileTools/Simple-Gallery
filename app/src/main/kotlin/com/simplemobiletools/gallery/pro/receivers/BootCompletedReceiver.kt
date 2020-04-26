74cd554248f9a1f2ba005af2fa8136c0f1ad0157
tibbi <tibor@kaputa.sk>
2020-04-26 18:45:06 +0200

    run media fetcher on reboot on a background thread

--- app/src/main/kotlin/com/simplemobiletools/gallery/pro/receivers/BootCompletedReceiver.kt
+++ app/src/main/kotlin/com/simplemobiletools/gallery/pro/receivers/BootCompletedReceiver.kt
@@ -3,13 +3,16 @@ package com.simplemobiletools.gallery.pro.receivers
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import com.simplemobiletools.commons.helpers.ensureBackgroundThread
 import com.simplemobiletools.gallery.pro.extensions.updateDirectoryPath
 import com.simplemobiletools.gallery.pro.helpers.MediaFetcher
 
 class BootCompletedReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
-        MediaFetcher(context).getFoldersToScan().forEach {
-            context.updateDirectoryPath(it)
+        ensureBackgroundThread {
+            MediaFetcher(context).getFoldersToScan().forEach {
+                context.updateDirectoryPath(it)
+            }
         }
     }
 }
