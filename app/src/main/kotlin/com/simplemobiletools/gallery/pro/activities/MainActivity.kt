49c11880235b7ee21eb489581688327cfd609ead
tibbi <tibor@kaputa.sk>
2020-06-07 17:46:21 +0200

    update commons to 5.29.0

--- app/src/main/kotlin/com/simplemobiletools/gallery/pro/activities/MainActivity.kt
+++ app/src/main/kotlin/com/simplemobiletools/gallery/pro/activities/MainActivity.kt
@@ -1347,7 +1347,7 @@ class MainActivity : SimpleActivity(), DirectoryOperationsListener {
                     }
                 }
             }
-        } catch (e: Exception) {
+        } catch (ignored: Exception) {
         }
 
         return folders
