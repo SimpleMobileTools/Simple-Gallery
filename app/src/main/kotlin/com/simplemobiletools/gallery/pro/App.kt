d068135a39a14380f46bbdadce1104542d0bedee
tibbi <tibor@kaputa.sk>
2021-03-30 15:16:41 +0200

    shortening some Picasso initialization code

--- app/src/main/kotlin/com/simplemobiletools/gallery/pro/App.kt
+++ app/src/main/kotlin/com/simplemobiletools/gallery/pro/App.kt
@@ -5,6 +5,7 @@ import com.github.ajalt.reprint.core.Reprint
 import com.simplemobiletools.commons.extensions.checkUseEnglish
 import com.squareup.picasso.Downloader
 import com.squareup.picasso.Picasso
+import okhttp3.Request
 import okhttp3.Response
 
 class App : Application() {
@@ -12,14 +13,10 @@ class App : Application() {
         super.onCreate()
         checkUseEnglish()
         Reprint.initialize(this)
-        Picasso.setSingletonInstance(Picasso.Builder(this).downloader(object: Downloader {
-            override fun load(request: okhttp3.Request): Response {
-                return Response.Builder().build()
-            }
+        Picasso.setSingletonInstance(Picasso.Builder(this).downloader(object : Downloader {
+            override fun load(request: Request) = Response.Builder().build()
 
-            override fun shutdown() {
-
-            }
+            override fun shutdown() {}
         }).build())
     }
 }
