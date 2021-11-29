a67e5ff86fe385eb080b5ff73d22cfc5fce396c1
tibbi <tibor@kaputa.sk>
2021-11-29 21:51:17 +0100

    properly handle margins between thumbnails, if selected so

--- app/src/main/kotlin/com/simplemobiletools/gallery/pro/activities/MediaActivity.kt
+++ app/src/main/kotlin/com/simplemobiletools/gallery/pro/activities/MediaActivity.kt
@@ -759,7 +759,7 @@ class MediaActivity : SimpleActivity(), MediaOperationsListener {
             val spacing = config.thumbnailSpacing
             val useGridPosition = media.firstOrNull() is ThumbnailSection
 
-            /*var currentGridDecoration: GridSpacingItemDecoration? = null
+            var currentGridDecoration: GridSpacingItemDecoration? = null
             if (media_grid.itemDecorationCount > 0) {
                 currentGridDecoration = media_grid.getItemDecorationAt(0) as GridSpacingItemDecoration
                 currentGridDecoration.items = media
@@ -771,7 +771,7 @@ class MediaActivity : SimpleActivity(), MediaOperationsListener {
                     media_grid.removeItemDecoration(currentGridDecoration)
                 }
                 media_grid.addItemDecoration(newGridDecoration)
-            }*/
+            }
         }
     }
 
