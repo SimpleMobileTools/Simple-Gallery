6518d264dcd78c3d7bf174382df516dfd1c4a0c3
Doozy <dozdoozie@gmail.com>
2022-07-06 03:21:27 +0300

    [feat] - Replace original file after edit

--- app/src/main/kotlin/com/simplemobiletools/gallery/pro/dialogs/SaveAsDialog.kt
+++ app/src/main/kotlin/com/simplemobiletools/gallery/pro/dialogs/SaveAsDialog.kt
@@ -1,5 +1,6 @@
 package com.simplemobiletools.gallery.pro.dialogs
 
+import android.view.View
 import androidx.appcompat.app.AlertDialog
 import com.simplemobiletools.commons.activities.BaseSimpleActivity
 import com.simplemobiletools.commons.dialogs.ConfirmationDialog
@@ -19,6 +20,16 @@ class SaveAsDialog(
         var realPath = path.getParentPath()
         if (activity.isRestrictedWithSAFSdk30(realPath) && !activity.isInDownloadDir(realPath)) {
             realPath = activity.getPicturesDirectoryPath(realPath)
+        }
+
+        val alertDialogBuilder = AlertDialog.Builder(activity)
+        var alertDialog: AlertDialog? = null
+        val originalFullName = when {
+            appendFilename -> path.getFilenameFromPath()
+            else           -> {
+                val tempFullName = path.getFilenameFromPath()
+                tempFullName.substring(0, tempFullName.lastIndexOf(".")).dropLast("_1".length) + tempFullName.substring(tempFullName.lastIndexOf("."))
+            }
         }
 
         val view = activity.layoutInflater.inflate(R.layout.dialog_save_as, null).apply {
@@ -46,60 +57,87 @@ class SaveAsDialog(
                     realPath = it
                 }
             }
-        }
-
-        AlertDialog.Builder(activity)
-            .setPositiveButton(R.string.ok, null)
-            .setNegativeButton(R.string.cancel) { dialog, which -> cancelCallback?.invoke() }
-            .setOnCancelListener { cancelCallback?.invoke() }
-            .create().apply {
-                activity.setupDialogStuff(view, this, R.string.save_as) {
-                    showKeyboard(view.save_as_name)
-                    getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener {
-                        val filename = view.save_as_name.value
-                        val extension = view.save_as_extension.value
-
-                        if (filename.isEmpty()) {
-                            activity.toast(R.string.filename_cannot_be_empty)
-                            return@setOnClickListener
-                        }
-
-                        if (extension.isEmpty()) {
-                            activity.toast(R.string.extension_cannot_be_empty)
-                            return@setOnClickListener
-                        }
-
-                        val newFilename = "$filename.$extension"
-                        val newPath = "${realPath.trimEnd('/')}/$newFilename"
-                        if (!newFilename.isAValidFilename()) {
-                            activity.toast(R.string.filename_invalid_characters)
-                            return@setOnClickListener
-                        }
-
-                        if (activity.getDoesFilePathExist(newPath)) {
-                            val title = String.format(activity.getString(R.string.file_already_exists_overwrite), newFilename)
-                            ConfirmationDialog(activity, title) {
-                                val newFile = File(newPath)
-                                val isInDownloadDir = activity.isInDownloadDir(newPath)
-                                val isInSubFolderInDownloadDir = activity.isInSubFolderInDownloadDir(newPath)
-                                if ((isRPlus() && !isExternalStorageManager()) && isInDownloadDir && !isInSubFolderInDownloadDir && !newFile.canWrite()) {
-                                    val fileDirItem = arrayListOf(File(newPath).toFileDirItem(activity))
-                                    val fileUris = activity.getFileUrisFromFileDirItems(fileDirItem)
-                                    activity.updateSDK30Uris(fileUris) { success ->
-                                        if (success) {
-                                            selectPath(this, newPath)
-                                        }
-                                    }
-                                } else {
-                                    selectPath(this, newPath)
-                                }
-                            }
-                        } else {
-                            selectPath(this, newPath)
-                        }
+            replace_original.setOnCheckedChangeListener { _, isChecked ->
+                run {
+                    toggleViewFocusability(save_as_name)
+                    toggleViewFocusability(save_as_path)
+                    toggleViewFocusability(save_as_extension)
+                    if (isChecked) {
+                        alertDialog?.getButton(AlertDialog.BUTTON_POSITIVE)?.text = context.getString(R.string.pesdk_sticker_button_replace)
+                    } else {
+                        alertDialog?.getButton(AlertDialog.BUTTON_POSITIVE)?.text = context.getString(R.string.ok)
                     }
                 }
             }
+        }
+
+        alertDialogBuilder
+            .setPositiveButton(R.string.ok, null)
+            .setNegativeButton(R.string.cancel) { dialog, which -> cancelCallback?.invoke() }
+            .setOnCancelListener { cancelCallback?.invoke() }
+        alertDialog = alertDialogBuilder.create()
+        alertDialog.apply {
+            activity.setupDialogStuff(view, this, R.string.save_as) {
+                showKeyboard(view.save_as_name)
+                getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener {
+                    val filename = view.save_as_name.value
+                    val extension = view.save_as_extension.value
+
+                    if (view.replace_original.isChecked) {
+                        overwriteFile("${realPath.trimEnd('/')}/$originalFullName")
+                        return@setOnClickListener
+                    }
+
+                    if (filename.isEmpty()) {
+                        activity.toast(R.string.filename_cannot_be_empty)
+                        return@setOnClickListener
+                    }
+                    if (extension.isEmpty()) {
+                        activity.toast(R.string.extension_cannot_be_empty)
+                        return@setOnClickListener
+                    }
+
+                    val newFilename = "$filename.$extension"
+                    val newPath = "${realPath.trimEnd('/')}/$newFilename"
+                    if (!newFilename.isAValidFilename()) {
+                        activity.toast(R.string.filename_invalid_characters)
+                        return@setOnClickListener
+                    }
+
+                    if (activity.getDoesFilePathExist(newPath)) {
+                        val title = String.format(activity.getString(R.string.file_already_exists_overwrite), newFilename)
+                        ConfirmationDialog(activity, title) {
+                            overwriteFile(newPath)
+                        }
+                    } else {
+                        selectPath(this, newPath)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun AlertDialog.overwriteFile(path: String) {
+        val newFile = File(path)
+        val isInDownloadDir = activity.isInDownloadDir(path)
+        val isInSubFolderInDownloadDir = activity.isInSubFolderInDownloadDir(path)
+        if (isRPlus() && isInDownloadDir && !isInSubFolderInDownloadDir && !newFile.canWrite()) {
+            val fileDirItem = arrayListOf(File(path).toFileDirItem(activity))
+            val fileUris = activity.getFileUrisFromFileDirItems(fileDirItem)
+            activity.updateSDK30Uris(fileUris) { success ->
+                if (success) {
+                    selectPath(this, path)
+                }
+            }
+        } else {
+            selectPath(this, path)
+        }
+    }
+
+    private fun toggleViewFocusability(view: View?) {
+        val viewIsEnabled = view?.isEnabled
+        view?.isEnabled = !viewIsEnabled!!
+        view.isFocusableInTouchMode = !viewIsEnabled
     }
 
     private fun selectPath(alertDialog: AlertDialog, newPath: String) {
