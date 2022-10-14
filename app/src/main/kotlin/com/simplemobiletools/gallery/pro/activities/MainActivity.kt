d5d2c9e2782372ea0d31afa01bf4f5f171f11469
tibbi <tibor@kaputa.sk>
2022-10-14 18:19:38 +0200

    always request All files access, but be satisfied with Media access only too

--- app/src/main/kotlin/com/simplemobiletools/gallery/pro/activities/MainActivity.kt
+++ app/src/main/kotlin/com/simplemobiletools/gallery/pro/activities/MainActivity.kt
@@ -1500,7 +1500,6 @@ class MainActivity : SimpleActivity(), DirectoryOperationsListener {
             add(Release(277, R.string.release_277))
             add(Release(295, R.string.release_295))
             add(Release(327, R.string.release_327))
-            add(Release(359, R.string.faq_16_text))
             add(Release(369, R.string.release_369))
             checkWhatsNew(this, BuildConfig.VERSION_CODE)
         }
