c7c25557de7e9e5e367dc037b323512611c5fbb3
tibbi <tibor@kaputa.sk>
2022-10-16 22:05:49 +0200

    correcting a view refetence

--- app/src/main/kotlin/com/simplemobiletools/gallery/pro/dialogs/AllFilesPermissionDialog.kt
+++ app/src/main/kotlin/com/simplemobiletools/gallery/pro/dialogs/AllFilesPermissionDialog.kt
@@ -1,11 +1,11 @@
 package com.simplemobiletools.gallery.pro.dialogs
 
+import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 import com.simplemobiletools.commons.activities.BaseSimpleActivity
 import com.simplemobiletools.commons.extensions.getAlertDialogBuilder
 import com.simplemobiletools.commons.extensions.setupDialogStuff
 import com.simplemobiletools.gallery.pro.R
-import kotlinx.android.synthetic.main.dialog_confirm_delete_folder.view.*
 
 class AllFilesPermissionDialog(
     val activity: BaseSimpleActivity, message: String = "", val callback: (result: Boolean) -> Unit, val neutralPressed: () -> Unit
@@ -14,7 +14,7 @@ class AllFilesPermissionDialog(
 
     init {
         val view = activity.layoutInflater.inflate(R.layout.dialog_message, null)
-        view.message.text = message
+        view.findViewById<TextView>(R.id.message).text = message
 
         activity.getAlertDialogBuilder().setPositiveButton(R.string.all_files) { dialog, which -> positivePressed() }
             .setNeutralButton(R.string.media_only) { dialog, which -> neutralPressed() }
