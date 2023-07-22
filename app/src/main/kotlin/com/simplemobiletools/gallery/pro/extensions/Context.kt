a9210c5a1bfebb06b11203258a78aad72121c5bf
yangfeng <1719957182@qq.com>
2023-07-22 20:02:08 +0800

    fix: lack of import statement

--- app/src/main/kotlin/com/simplemobiletools/gallery/pro/extensions/Context.kt
+++ app/src/main/kotlin/com/simplemobiletools/gallery/pro/extensions/Context.kt
@@ -23,6 +23,7 @@ import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.request.target.Target
 import com.bumptech.glide.signature.ObjectKey
 import com.simplemobiletools.commons.extensions.*
 import com.simplemobiletools.commons.helpers.*
