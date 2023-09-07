13faec0af1f3fcb1848ea9eeb3a94d465b1bee2d
Tibor Kaputa <tibor@kaputa.sk>
2023-09-07 09:37:18 +0200

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,12 +17,14 @@ dependencyResolutionManagement {
         maven(url = "https://artifactory.img.ly/artifactory/imgly")
     }
 }
-// TODO: This will be deprecated in future. Migrate to the newer `pluginManagement { includeBuild() }` mechanism instead of explicitly substituting dependency.
-//includeBuild("../Simple-Commons") {
-//    dependencySubstitution {
-//        substitute(module("com.github.SimpleMobileTools:Simple-Commons")).using(project(":commons"))
-//    }
-//}
+
 rootProject.name = "Simple-Gallery"
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 include(":app")
+
+// TODO: This will be deprecated in future. Migrate to the newer `pluginManagement { includeBuild() }` mechanism instead of explicitly substituting dependency.
+/*includeBuild("../Simple-Commons") {
+    dependencySubstitution {
+        substitute(module("com.github.SimpleMobileTools:Simple-Commons")).using(project(":commons"))
+    }
+}*/
